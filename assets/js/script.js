5ebca7669cb3ea6f0ee9cd46a7bf1802ea1ac5de
Ufuoma Ekemike <uekemike@yahoo.com>
2021-07-20 00:35:13 -0400

    more changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,21 +11,21 @@ var timeleft =10;
 var questions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
-        a: '<scripting>',
-        b: '<js>',
-        c: '<script>',
+        a: "scripting",
+        b: "js",
+        c: "script",
         answer: "C"
       },
     {
         question: "Where is the correct place to insert a JavaScript?",     
-        a: 'The <body> section',
-        b: 'The <head> section',
-        c: 'Both the <head> section and the <body> section are correct',
+        a: 'The body section',
+        b: 'The head section',
+        c: 'Both the head section and the body section are correct',
         answer: "C"
       },
 
     {
-        question: "The external JavaScript file must contain the <script> tag.",
+        question: "The external JavaScript file must contain the script tag.",
         a: 'True',
         b: 'False',
         c: 'Or both',
